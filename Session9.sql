b66306046c50192fd743eec87f6d0955fd760b1e
AshlinJC <23BAI70722@cuchd.in>
2026-06-24 09:47:11 +0530

    Create Session9.sql

--- Session9.sql
+++ Session9.sql
@@ -1,3 +1,5 @@
+--TEST
+
 --Average Review Ratings
 
 SELECT 
@@ -9,3 +11,43 @@ GROUP BY
   EXTRACT(MONTH FROM submit_date), 
   product_id
 ORDER BY mth, product_id;
+
+--Y-on-Y Growth Rate
+
+WITH yearly_spend_cte AS (
+  SELECT 
+    EXTRACT(YEAR FROM transaction_date) AS year,
+    product_id,
+    spend AS curr_year_spend,
+    LAG(spend) OVER (
+      PARTITION BY product_id 
+      ORDER BY 
+        product_id, 
+        EXTRACT(YEAR FROM transaction_date)) AS prev_year_spend 
+  FROM user_transactions
+)
+
+SELECT 
+  year,
+  product_id, 
+  curr_year_spend, 
+  prev_year_spend, 
+  ROUND(100 * 
+    (curr_year_spend - prev_year_spend)
+    / prev_year_spend
+  , 2) AS yoy_rate 
+FROM yearly_spend_cte;
+
+--Median Google Search Frequency
+
+WITH searches_expanded AS (
+  SELECT searches
+  FROM search_frequency
+  GROUP BY 
+    searches, 
+    GENERATE_SERIES(1, num_users))
+
+SELECT 
+  ROUND(PERCENTILE_CONT(0.50) WITHIN GROUP (
+    ORDER BY searches)::DECIMAL, 1) AS  median
+FROM searches_expanded;
